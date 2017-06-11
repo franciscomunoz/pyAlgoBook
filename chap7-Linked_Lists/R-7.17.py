0a177a69424be94c7f38badffc7e9c3fe0bd6c4d
Francisco Missael Munoz Ruiz <francisco.missael@gmail.com>
2017-06-11 19:24:40 -0400

    problems 7.17 7.18 7.19 7.20 7.22 7.23

---
+++ chap7-Linked_Lists/R-7.17.py
@@ -0,0 +1,9 @@
+"""Augment the PositionalList class to support a new method, move_to_front(p),
+that accomplishes this goal more directly, by reliking the existing node."""
+
+Solution :
+
+    Implemented in positional_list.py move_to_front
+    doubly_linked_base.py _move_to_front and 
+    favorites_list_mtf.py
+ 
